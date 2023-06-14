b7f0bb97f7e66dbc7c27563a079015e2cc1e4496
Harshit <harshitchugh001@gmail.com>
2023-06-14 17:29:10 +0530

    Password_change done

--- app/src/main/java/com/example/gjucampus/Password_change.kt
+++ app/src/main/java/com/example/gjucampus/Password_change.kt
@@ -5,55 +5,73 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.EmailAuthProvider
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Password_change.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Password_change : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var etCurrentPassword: EditText
+    private lateinit var etNewPassword: EditText
+    private lateinit var etConfirmPassword: EditText
+    private lateinit var btnChangePassword: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_password_change, container, false)
+        val view = inflater.inflate(R.layout.fragment_password_change, container, false)
+
+        etCurrentPassword = view.findViewById(R.id.current_pass)
+        etNewPassword = view.findViewById(R.id.update_pass)
+        etConfirmPassword = view.findViewById(R.id.confirmPassEt)
+        btnChangePassword = view.findViewById(R.id.button)
+
+        btnChangePassword.setOnClickListener {
+            val currentPassword = etCurrentPassword.text.toString()
+            val newPassword = etNewPassword.text.toString()
+            val confirmPassword = etConfirmPassword.text.toString()
+
+            if (newPassword == confirmPassword) {
+                changePassword(currentPassword, newPassword)
+            } else {
+                showToast("New password and confirm password do not match")
+            }
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Password_change.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Password_change().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun changePassword(currentPassword: String, newPassword: String) {
+        val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        val credential = user?.email?.let {
+            EmailAuthProvider.getCredential(it, currentPassword)
+        }
+
+        if (credential != null) {
+            user?.reauthenticate(credential)?.addOnCompleteListener { reauthTask ->
+                if (reauthTask.isSuccessful) {
+                    user.updatePassword(newPassword).addOnCompleteListener { updateTask ->
+                        if (updateTask.isSuccessful) {
+                            showToast("Password updated successfully")
+                        } else {
+                            val exception = updateTask.exception as? FirebaseAuthException
+                            showToast("Password update failed: ${exception?.message}")
+                        }
+                    }
+                } else {
+                    showToast("Reauthentication failed")
                 }
             }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
